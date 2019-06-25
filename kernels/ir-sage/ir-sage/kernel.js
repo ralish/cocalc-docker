16daba55da622c8f7395ce6d14a979672c9543f5
Samuel D. Leslie <sdl@nexiom.net>
2019-06-25 12:38:22 +1000

    Trim some extra line endings & whitespace

--- kernels/ir-sage/ir-sage/kernel.js
+++ kernels/ir-sage/ir-sage/kernel.js
@@ -8,10 +8,10 @@ const extra_map = (notebook, cell) => ({
 	F1(cm) {
 		const {anchor, head} = cm.findWordAt(cm.getCursor())
 		const word = cm.getRange(anchor, head)
-		
+
 		const callbacks = cell.get_callbacks()
 		const options = {silent: false, store_history: false, stop_on_error: true}
-		cell.last_msg_id = notebook.kernel.execute(`help(${word})`, callbacks, options) 
+		cell.last_msg_id = notebook.kernel.execute(`help(${word})`, callbacks, options)
 	},
 })
 
@@ -26,7 +26,7 @@ define(['base/js/namespace'], ({notebook}) => ({
 	onload() {
 		for (let cell of notebook.get_cells())
 			enable_extra_keys(notebook, cell)
-		
+
 		notebook.events.on('edit_mode.Cell', (event, {cell}) =>
 			enable_extra_keys(notebook, cell))
 	},
