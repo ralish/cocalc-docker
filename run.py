21360cc7f95b0185c57d9db5154bd71e167d3081
Samuel D. Leslie <sdl@nexiom.net>
2019-10-01 12:18:26 +1000

    Clean-up of run.py to make the linter happy

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import os
-import shutil
 import subprocess
 import sys
 import tempfile
@@ -9,7 +8,7 @@ import time
 
 # Where the PostgreSQL data is stored
 PGDATA = '/projects/postgres/data'
-PGHOST  = os.path.join(PGDATA, 'socket')
+PGHOST = os.path.join(PGDATA, 'socket')
 os.environ['PGHOST'] = PGHOST
 os.environ['PGUSER'] = 'smc'
 
@@ -18,9 +17,11 @@ os.umask(0o077)
 
 join = os.path.join
 
+
 def log(*args):
     print(*args)
     sys.stdout.flush()
+
 
 def run(v, shell=False, path='.', get_output=False, env=None, verbose=1):
     t = time.time()
@@ -28,19 +29,19 @@ def run(v, shell=False, path='.', get_output=False, env=None, verbose=1):
         cmd = v
         shell = True
     else:
-        cmd = ' '.join([(x if len(x.split())<=1 else '"%s"'%x) for x in v])
+        cmd = ' '.join([(x if len(x.split()) <= 1 else '"%s"' % x) for x in v])
     if path != '.':
         cur = os.path.abspath(os.curdir)
         if verbose:
-            print('chdir %s'%path)
+            print('chdir %s' % path)
         os.chdir(path)
     try:
         if verbose:
             print(cmd)
         if shell:
-            kwds = {'shell':True, 'executable':'/bin/bash', 'env':env}
+            kwds = {'shell': True, 'executable': '/bin/bash', 'env': env}
         else:
-            kwds = {'env':env}
+            kwds = {'env': env}
         if get_output:
             output = subprocess.Popen(v, stdout=subprocess.PIPE, **kwds).stdout.read().decode()
         else:
@@ -55,7 +56,8 @@ def run(v, shell=False, path='.', get_output=False, env=None, verbose=1):
         if path != '.':
             os.chdir(cur)
 
-def self_signed_cert(target= 'nopassphrase.pem'):
+
+def self_signed_cert(target='nopassphrase.pem'):
     if os.path.exists('/projects/conf/nopassphrase.pem'):
         log("installing cert at '/projects/conf/nopassphrase.pem'")
         run("cp /projects/conf/nopassphrase.pem {target} && chmod og-rwx {target}".format(target=target))
@@ -63,11 +65,12 @@ def self_signed_cert(target= 'nopassphrase.pem'):
     log("create self_signed_cert")
     with tempfile.TemporaryDirectory() as tmp:
         run(['openssl', 'req', '-new', '-x509', '-nodes', '-out', 'server.crt',
-                  '-keyout', 'server.key',
-                  '-subj', '/C=US/ST=WA/L=WA/O=Network/OU=IT Department/CN=sagemath'], path=tmp)
-        s  = open(join(tmp, 'server.crt')).read() + open(join(tmp, 'server.key')).read()
-        open(target,'w').write(s)
+            '-keyout', 'server.key',
+            '-subj', '/C=US/ST=WA/L=WA/O=Network/OU=IT Department/CN=sagemath'], path=tmp)
+        s = open(join(tmp, 'server.crt')).read() + open(join(tmp, 'server.key')).read()
+        open(target, 'w').write(s)
         run("chmod og-rwx {target} && mkdir -p /projects/conf && cp {target} /projects/conf/nopassphrase.pem".format(target=target))
+
 
 def init_projects_path():
     log("initialize /projects path")
@@ -81,7 +84,8 @@ def init_projects_path():
         if not os.path.exists(full_path):
             log("creating ", full_path)
             os.makedirs(full_path)
-            run("chmod og-rwx '%s'"%full_path)
+            run("chmod og-rwx '%s'" % full_path)
+
 
 def start_services():
     for name in ['haproxy', 'nginx', 'ssh']:
@@ -93,6 +97,7 @@ def root_ssh_keys():
     run("ssh-keygen -t ecdsa -N '' -f /root/.ssh/id_ecdsa")
     run("cp -v /root/.ssh/id_ecdsa.pub /root/.ssh/authorized_keys")
 
+
 def start_hub():
     run(". smc-env && hub start \
             --host=localhost \
@@ -103,28 +108,31 @@ def start_hub():
             --update \
             --single \
             --logfile /var/log/hub.log \
-            --pidfile /run/hub.pid &", \
+            --pidfile /run/hub.pid &",
         path='/cocalc/src')
+
 
 def postgres_perms():
     run("mkdir -p /projects/postgres && chown -R sage. /projects/postgres && chmod og-rwx -R /projects/postgres")
 
+
 def start_postgres():
     postgres_perms()
     if not os.path.exists(PGDATA):  # see comments in smc/src/dev/project/start_postgres.py
-        run("sudo -u sage /usr/lib/postgresql/10/bin/pg_ctl init -D '%s'"%PGDATA)
-        open(os.path.join(PGDATA,'pg_hba.conf'), 'w').write("local all all trust")
+        run("sudo -u sage /usr/lib/postgresql/10/bin/pg_ctl init -D '%s'" % PGDATA)
+        open(os.path.join(PGDATA, 'pg_hba.conf'), 'w').write("local all all trust")
         conf = os.path.join(PGDATA, 'postgresql.conf')
-        s = open(conf).read() + "\nunix_socket_directories = '%s'\nlisten_addresses=''\n"%PGHOST
-        open(conf,'w').write(s)
+        s = open(conf).read() + "\nunix_socket_directories = '%s'\nlisten_addresses=''\n" % PGHOST
+        open(conf, 'w').write(s)
         os.makedirs(PGHOST)
         postgres_perms()
-        run("sudo -u sage /usr/lib/postgresql/10/bin/postgres -D '%s' >%s/postgres.log 2>&1 &"%(PGDATA, PGDATA))
+        run("sudo -u sage /usr/lib/postgresql/10/bin/postgres -D '%s' >%s/postgres.log 2>&1 &" % (PGDATA, PGDATA))
         time.sleep(5)
-        run("sudo -u sage /usr/lib/postgresql/10/bin/createuser -h '%s' -sE smc"%PGHOST)
-        run("sudo -u sage kill %s"%(open(os.path.join(PGDATA, 'postmaster.pid')).read().split()[0]))
+        run("sudo -u sage /usr/lib/postgresql/10/bin/createuser -h '%s' -sE smc" % PGHOST)
+        run("sudo -u sage kill %s" % (open(os.path.join(PGDATA, 'postmaster.pid')).read().split()[0]))
         time.sleep(3)
-    os.system("sudo -u sage /usr/lib/postgresql/10/bin/postgres -D '%s' > /var/log/postgres.log 2>&1 &"%PGDATA)
+    os.system("sudo -u sage /usr/lib/postgresql/10/bin/postgres -D '%s' > /var/log/postgres.log 2>&1 &" % PGDATA)
+
 
 def reset_project_state():
     while True:
@@ -134,6 +142,7 @@ def reset_project_state():
         except:
             time.sleep(1)
 
+
 def start_compute():
     # We always delete compute.sqlite3 (resetting it) since obviously all projects are stopped on container startup.
     run("mkdir -p /projects/conf && chmod og-rwx -R /projects/conf && rm -f /projects/conf/compute.sqlite3")
@@ -142,8 +151,10 @@ def start_compute():
     # TODO: should really do this right -- since if the compute-client tries to initialize schema at same, time things get hosed.
     run("""sleep 15; . smc-env; echo "require('ts-node').register(); require('smc-hub/compute-client').compute_server(cb:(e,s)-> s._add_server_single(cb:->process.exit(0)))" | coffee & """, path='/cocalc/src')
 
+
 def tail_logs():
     run("tail -f /var/log/compute.log /var/log/compute.err /cocalc/logs/*")
+
 
 def main():
     self_signed_cert('/run/haproxy.pem')
@@ -156,6 +167,7 @@ def main():
     reset_project_state()
     while True:
         os.wait()
+
 
 if __name__ == "__main__":
     main()
